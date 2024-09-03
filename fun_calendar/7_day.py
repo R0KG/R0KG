ede2cf21eff0ca9808aad1befa0a8618fd826f01
R0KG <int1994019@gmail.com>
2024-09-03 23:19:47 +0200

    hmm

--- fun_calendar/7_day.py
+++ fun_calendar/7_day.py
@@ -1,4 +1,27 @@
 
+
+
+import re
+from collections import Counter
+
+def calculate_total_winnings(hands, card_order, joker_transform=False):
+    def sort_key(hand):
+        if joker_transform and 'J' in hand:
+            highest_card = max((card for card in hand if card != 'J'), key=lambda c: card_order.index(c), default='2')
+            hand = hand.replace('J', Counter(hand.replace('J', '')).most_common(1)[0][0] if hand.count('J') < 5 else highest_card)
+        return (tuple(sorted(Counter(hand).values(), reverse=True)), tuple(card_order.index(c) for c in hand))
+
+    return sum(w * (i + 1) for i, h in enumerate(sorted(hands.keys(), key=sort_key)) for w in [hands[h]])
+
+def load_hands(file_path):
+    with open(file_path, 'r') as file:
+        return {h: int(b) for h, b in re.findall(r'(\w{5}) (\d+)', file.read())}
+
+
+file_path = ''
+hands = load_hands(file_path)
+print("Total Winnings Part 1:", calculate_total_winnings(hands, '23456789TJQKA'))
+print("Total Winnings Part 2:", calculate_total_winnings(hands, 'J23456789TQKA', joker_transform=True))
 import re
 
 # Bool for true/false if it´s a combination 
@@ -107,20 +130,35 @@ def internal_sort(inp):
     for comb in inp:
         temp_list = []
         min_letter = temp[0]
-        for word in comb:
-            temp = comb[0]
-            if temp < min_letter:
-                min_letter = temp
-        for word in comb:
-            temp_word = word.split()
-            if temp_word.startswith(min_letter):
-                temp_list.append(word)
-        
-        if len(temp_list) == 1:
-            list.append(temp_list)
-        else:
-            for word in temp_list:
-                
+        while(len(comb) > 0):
+            for word in comb:
+                temp = word[0]
+                if temp < min_letter:
+                    min_letter = temp
+            for word in comb:
+                temp_word = word.split()
+                if temp_word.startswith(min_letter):
+                    temp_list.append(word)
+            
+            if len(temp_list) == 1:
+                list.append(temp_list)
+            else:
+                list_pairs = []
+                for word in temp_list:
+                    min_letter = word[1]
+                    for str in temp_list:
+                        if str[1] < min_letter:
+                            min_letter = str[1]
+                    sort_l = []
+                    for word in temp_list:
+                        temp_word = word.split()
+                        if temp_word.startswith(min_letter):
+                            sort_l.append(word)
+                    temp_sort = sort_l[0]
+                    
+                        
+                    
+                        
                     
 
             
